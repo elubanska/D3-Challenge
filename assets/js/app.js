10a9b14e45fb688ceb8795df26eaa0b31e39ba9b
Emilia <e.lubanska@gmail.com>
2021-01-27 21:35:14 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,8 @@ var height = svgHeight - margin.top - margin.bottom;
 // Create an SVG wrapper, append an SVG group that will hold our chart,
 // and shift the latter by left and top margins.
 var svg = d3
-  .select(".chart")
+  //.select(".chart")
+  .select(".article")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
